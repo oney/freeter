fad6b7d74aa199965fdd070505018983e3c58b4d
One <appdevoney@gmail.com>
2015-03-11 02:10:48 +0800

    initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Freeterw
       end
     end
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
